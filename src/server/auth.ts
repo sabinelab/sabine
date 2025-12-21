47545e64a8e4415a910296c1ed14414af4aafe8c
levisantosp <contatowizard01@gmail.com>
2025-12-21 13:28:19 -0300

    fix: auth

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -3,7 +3,10 @@ import { env } from '@/env'
 
 export const auth = new Elysia()
   .onBeforeHandle({ as: 'scoped' }, ({ headers, set }) => {
-    if(headers.authorization !== env.AUTH) {
+    if(
+      headers.authorization !== env.AUTH ||
+      headers.Authorization !== env.AUTH
+    ) {
       set.status = 'Unauthorized'
 
       return { message: 'Unauthorized' }
