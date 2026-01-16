dd1be626fd82ddde22fc5bd6b95cde3fd2d6d7a0
levisantosp <levi.pires2005@gmail.com>
2026-01-16 16:30:29 -0300

    perf: optimize transaction retrieval and pagination logic

--- src/commands/economy/transactions.ts
+++ src/commands/economy/transactions.ts
@@ -30,111 +30,19 @@ export default createCommand({
   async run({ ctx, t, app }) {
     const page = Number(ctx.args[0]) || 1
 
-    let transactions = (
-      await app.prisma.transaction.findMany({
-        where: {
-          profile: {
-            userId: ctx.db.profile.userId,
-            guildId: ctx.db.guild.id
-          }
+    const transactions = await app.prisma.transaction.findMany({
+      where: {
+        profile: {
+          userId: ctx.db.profile.userId,
+          guildId: ctx.db.guild.id
         }
-      })
-    ).sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())
-
-    const array = transactions
-
-    if (page === 1) {
-      transactions = transactions.slice(0, 10)
-    } else transactions = transactions.slice(page * 10 - 10, page * 10)
-    if (!transactions.length) {
-      return await ctx.reply('commands.transactions.none_yet')
-    }
-
-    const embed = new EmbedBuilder().setTitle(t('commands.transactions.embed.title')).setFooter({
-      text: t('commands.transactions.embed.footer', {
-        page,
-        pages: Math.ceil(array.length / 10)
-      })
+      },
+      orderBy: {
+        createdAt: 'desc'
+      },
+      skip: (page - 1) * 10,
+      take: 11
     })
-
-    let description = ''
-
-    for (const transaction of transactions) {
-      const timestamp = (transaction.createdAt.getTime() / 1000).toFixed(0)
-
-      if (transaction.player) {
-        const player = app.players.get(transaction.player.toString())
-
-        if (!player) continue
-
-        description += `- [<t:${timestamp}:d> <t:${timestamp}:t> | <t:${timestamp}:R>] ${t(
-          `commands.transactions.type.${transaction.type}`,
-          {
-            player: `${player.name} (${player.collection})`,
-            price: transaction.price?.toLocaleString(),
-            user: `<@${transaction.to}>`,
-            cmd: `</claim:${app.commands.get('claim')?.id}>`
-          }
-        )}\n`
-      } else if (transaction.pack) {
-        description += `- [<t:${timestamp}:d> <t:${timestamp}:t> | <t:${timestamp}:R>] ${t(
-          `commands.transactions.type.${transaction.type}`,
-          {
-            pack: transaction.pack
-          }
-        )}\n`
-      }
-    }
-
-    embed.setDesc(description)
-
-    const previous = new ButtonBuilder()
-      .defineStyle('blue')
-      .setEmoji('1404176223621611572')
-      .setCustomId(
-        `transactions;${ctx.interaction.user.id};${page - 1 < 1 ? 1 : page - 1};previous`
-      )
-
-    const next = new ButtonBuilder()
-      .defineStyle('blue')
-      .setEmoji('1404176291829121028')
-      .setCustomId(
-        `transactions;${ctx.interaction.user.id};${page + 1 > Math.ceil(array.length / 10) ? Math.ceil(array.length / 10) : page + 1};next`
-      )
-
-    if (page <= 1) previous.setDisabled()
-
-    if (page >= Math.ceil(array.length / 10)) next.setDisabled()
-    await ctx.reply(
-      embed.build({
-        components: [
-          {
-            type: 1,
-            components: [previous, next]
-          }
-        ]
-      })
-    )
-  },
-  async createMessageComponentInteraction({ ctx, t, app }) {
-    ctx.setFlags(64)
-
-    const page = Number(ctx.args[2])
-
-    let transactions = (
-      await app.prisma.transaction.findMany({
-        where: {
-          profile: {
-            userId: ctx.db.profile.userId,
-            guildId: ctx.db.guild.id
-          }
-        }
-      })
-    ).sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())
-
-    const array = transactions
-
-    transactions = transactions.slice(page * 10 - 10, page * 10)
 
     if (!transactions.length) {
       return await ctx.reply('commands.transactions.none_yet')
@@ -142,14 +50,13 @@ export default createCommand({
 
     const embed = new EmbedBuilder().setTitle(t('commands.transactions.embed.title')).setFooter({
       text: t('commands.transactions.embed.footer', {
-        page,
-        pages: Math.ceil(array.length / 10)
+        page
       })
     })
 
     let description = ''
 
-    for (const transaction of transactions) {
+    for (const transaction of transactions.slice(0, 10)) {
       const timestamp = (transaction.createdAt.getTime() / 1000).toFixed(0)
 
       if (transaction.player) {
@@ -182,14 +89,106 @@ export default createCommand({
       .defineStyle('blue')
       .setEmoji('1404176223621611572')
       .setCustomId(`transactions;${ctx.interaction.user.id};${page - 1};previous`)
+
     const next = new ButtonBuilder()
       .defineStyle('blue')
       .setEmoji('1404176291829121028')
       .setCustomId(`transactions;${ctx.interaction.user.id};${page + 1};next`)
 
-    if (page <= 1) previous.setDisabled()
+    if (page <= 1) {
+      previous.setDisabled()
+    }
+    if (transactions.length <= 10) {
+      next.setDisabled()
+    }
 
-    if (page >= Math.ceil(array.length / 10)) next.setDisabled()
+    await ctx.reply(
+      embed.build({
+        components: [
+          {
+            type: 1,
+            components: [previous, next]
+          }
+        ]
+      })
+    )
+  },
+  async createMessageComponentInteraction({ ctx, t, app }) {
+    ctx.setFlags(64)
+
+    const page = Number(ctx.args[2]) || 1
+
+    const transactions = await app.prisma.transaction.findMany({
+      where: {
+        profile: {
+          userId: ctx.db.profile.userId,
+          guildId: ctx.db.guild.id
+        }
+      },
+      orderBy: {
+        createdAt: 'desc'
+      },
+      skip: (page - 1) * 10,
+      take: 11
+    })
+
+    if (!transactions.length) {
+      return await ctx.reply('commands.transactions.none_yet')
+    }
+
+    const embed = new EmbedBuilder().setTitle(t('commands.transactions.embed.title')).setFooter({
+      text: t('commands.transactions.embed.footer', {
+        page
+      })
+    })
+
+    let description = ''
+
+    for (const transaction of transactions.slice(0, 10)) {
+      const timestamp = (transaction.createdAt.getTime() / 1000).toFixed(0)
+
+      if (transaction.player) {
+        const player = app.players.get(transaction.player.toString())
+
+        if (!player) continue
+
+        description += `- [<t:${timestamp}:d> <t:${timestamp}:t> | <t:${timestamp}:R>] ${t(
+          `commands.transactions.type.${transaction.type}`,
+          {
+            player: `${player.name} (${player.collection})`,
+            price: transaction.price?.toLocaleString(),
+            user: `<@${transaction.to}>`,
+            cmd: `</claim:${app.commands.get('claim')?.id}>`
+          }
+        )}\n`
+      } else if (transaction.pack) {
+        description += `- [<t:${timestamp}:d> <t:${timestamp}:t> | <t:${timestamp}:R>] ${t(
+          `commands.transactions.type.${transaction.type}`,
+          {
+            pack: transaction.pack
+          }
+        )}\n`
+      }
+    }
+
+    embed.setDesc(description)
+
+    const previous = new ButtonBuilder()
+      .defineStyle('blue')
+      .setEmoji('1404176223621611572')
+      .setCustomId(`transactions;${ctx.interaction.user.id};${page - 1};previous`)
+
+    const next = new ButtonBuilder()
+      .defineStyle('blue')
+      .setEmoji('1404176291829121028')
+      .setCustomId(`transactions;${ctx.interaction.user.id};${page + 1};next`)
+
+    if (page <= 1) {
+      previous.setDisabled()
+    }
+    if (transactions.length <= 10) {
+      next.setDisabled()
+    }
 
     await ctx.edit({
       embeds: [embed],
