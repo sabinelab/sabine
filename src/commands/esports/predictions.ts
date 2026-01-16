d122604afef5ca82ec7f1b4f628f14ef8fad52f6
levisantosp <levi.pires2005@gmail.com>
2026-01-16 16:54:58 -0300

    perf: improve predictions command by consolidating logic and enhancing pagination

--- src/commands/esports/predictions.ts
+++ src/commands/esports/predictions.ts
@@ -1,6 +1,13 @@
+import type { $Enums } from '@generated'
 import ButtonBuilder from '../../structures/builders/ButtonBuilder'
 import EmbedBuilder from '../../structures/builders/EmbedBuilder'
 import createCommand from '../../structures/command/createCommand'
+
+const status = {
+  correct: '<:success:1300882212190945292>',
+  incorrect: '<:error:1300882259078938685>',
+  pending: '<a:loading:809221866434199634>'
+} as const
 
 export default createCommand({
   name: 'predictions',
@@ -65,380 +72,191 @@ export default createCommand({
     'predictions valorant 5'
   ],
   async run({ ctx, t, app }) {
-    if (ctx.args[0] === 'valorant') {
-      const predictions = await app.prisma.prediction.findMany({
+    const page = Number(ctx.args[1]) || 1
+
+    const [predictions, count] = await Promise.all([
+      app.prisma.prediction.findMany({
         where: {
-          game: 'valorant',
-          profile: {
-            userId: ctx.db.profile.userId,
-            guildId: ctx.db.guild.id
-          }
+          game: ctx.args[0] as $Enums.Game,
+          profileId: ctx.db.profile.id
         },
         include: {
           teams: true
-        }
-      })
-      if (!predictions.length) {
-        return await ctx.reply('commands.predictions.no_predictions')
-      }
-      let preds = predictions.sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())
-      const page = !ctx.args[1] ? 1 : Number(ctx.args[1])
-      const pages = Math.ceil(preds.length / 5)
-      if (page === 1) preds = preds.slice(0, 5)
-      else preds = preds.slice(page * 5 - 5, page * 5)
-      if (!preds.length) {
-        return await ctx.reply('commands.predictions.no_pages')
-      }
-      const embed = new EmbedBuilder()
-        .setAuthor({
-          name: t('commands.predictions.embed.author'),
-          iconURL: ctx.interaction.user.displayAvatarURL({ size: 2048 })
-        })
-        .setDesc(
-          t('commands.predictions.embed.desc', {
-            correct: ctx.db.profile.correctPredictions,
-            wrong: ctx.db.profile.incorrectPredictions,
-            t: predictions.length
-          })
-        )
-        .setFooter({
-          text: t('commands.predictions.embed.footer', {
-            p1: Number.isNaN(page) ? 1 : page,
-            p2: pages
-          })
-        })
-      for (const prediction of preds) {
-        let status: string
-        let odd = ''
-        if (prediction.status === 'correct') {
-          status = '<:success:1300882212190945292>'
-        } else if (prediction.status === 'incorrect') {
-          status = '<:error:1300882259078938685>'
-        } else {
-          status = '<a:carregando:809221866434199634>'
-        }
-        if (prediction.odd) {
-          odd += `\nOdd: \`${prediction.odd}x\``
-        }
-        const timestamp = (prediction.createdAt.getTime() / 1000).toFixed(0)
-        embed.addField(
-          `${prediction.teams[0].name} <:versus:1349105624180330516> ${prediction.teams[1].name} (<t:${timestamp}:d> <t:${timestamp}:t> | <t:${timestamp}:R>)`,
-          t('commands.predictions.embed.field', {
-            score1: prediction.teams[0].score,
-            score2: prediction.teams[1].score,
-            link: `https://www.vlr.gg/${prediction.match}`,
-            status
-          }) + odd
-        )
-      }
-      const previous = new ButtonBuilder()
-        .setEmoji('1404176223621611572')
-        .setCustomId(
-          `predictions;${ctx.interaction.user.id};${page - 1 < 1 ? 1 : page - 1};previous;valorant`
-        )
-        .defineStyle('blue')
-      const next = new ButtonBuilder()
-        .setEmoji('1404176291829121028')
-        .setCustomId(
-          `predictions;${ctx.interaction.user.id};${page + 1 > pages ? pages : page + 1};next;valorant`
-        )
-        .defineStyle('blue')
-      if (page <= 1) previous.setDisabled()
-      if (page >= pages) next.setDisabled()
-      await ctx.reply({
-        embeds: [embed],
-        components: [
-          {
-            type: 1,
-            components: [previous, next]
-          }
-        ]
-      })
-    } else {
-      const predictions = await app.prisma.prediction.findMany({
-        where: {
-          game: 'lol',
-          profile: {
-            userId: ctx.db.profile.userId,
-            guildId: ctx.db.guild.id
-          }
         },
-        include: {
-          teams: true
+        orderBy: {
+          createdAt: 'desc'
+        },
+        skip: (page - 1) * 5,
+        take: 5
+      }),
+      app.prisma.prediction.count({
+        where: {
+          game: ctx.args[0] as $Enums.Game,
+          profileId: ctx.db.profile.id
         }
       })
-      if (!predictions.length) {
-        return await ctx.reply('commands.predictions.no_predictions')
-      }
-      let preds = predictions.sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())
-      const page = !ctx.args[1] ? 1 : Number(ctx.args[1])
-      const pages = Math.ceil(predictions.length / 5)
-      if (page === 1) preds = preds.slice(0, 5)
-      else preds = preds.slice(page * 5 - 5, page * 5)
-      if (!preds.length) {
-        return await ctx.reply('commands.predictions.no_pages')
-      }
-      const embed = new EmbedBuilder()
-        .setAuthor({
-          name: t('commands.predictions.embed.author'),
-          iconURL: ctx.interaction.user.displayAvatarURL({ size: 2048 })
-        })
-        .setDesc(
-          t('commands.predictions.embed.desc', {
-            correct: ctx.db.profile.correctPredictions,
-            wrong: ctx.db.profile.incorrectPredictions,
-            t: predictions.length
-          })
-        )
-        .setFooter({
-          text: t('commands.predictions.embed.footer', {
-            p1: Number.isNaN(page) ? 1 : page,
-            p2: pages
-          })
-        })
-      for (const prediction of preds) {
-        let status: string
-        let odd = ''
-        if (prediction.status === 'correct') {
-          status = '<:success:1300882212190945292>'
-        } else if (prediction.status === 'incorrect') {
-          status = '<:error:1300882259078938685>'
-        } else {
-          status = '<a:carregando:809221866434199634>'
-        }
-        if (prediction.odd) {
-          odd += `\nOdd: \`${prediction.odd}x\``
-        }
-        const timestamp = (prediction.createdAt.getTime() / 1000).toFixed(0)
-        embed.addField(
-          `${prediction.teams[0].name} <:versus:1349105624180330516> ${prediction.teams[1].name} (<t:${timestamp}:d> <t:${timestamp}:t> | <t:${timestamp}:R>)`,
-          t('commands.predictions.embed.field', {
-            score1: prediction.teams[0].score,
-            score2: prediction.teams[1].score,
-            link: `https://www.loltv.gg/match/${prediction.match}`,
-            status
-          }) + odd
-        )
-      }
-      const previous = new ButtonBuilder()
-        .setEmoji('1404176223621611572')
-        .setCustomId(
-          `predictions;${ctx.interaction.user.id};${page - 1 < 1 ? 1 : page - 1};previous;lol`
-        )
-        .defineStyle('blue')
-      const next = new ButtonBuilder()
-        .setEmoji('1404176291829121028')
-        .setCustomId(
-          `predictions;${ctx.interaction.user.id};${page + 1 > pages ? pages : page + 1};next;lol`
-        )
-        .defineStyle('blue')
-      if (page <= 1) previous.setDisabled()
-      if (page >= pages) next.setDisabled()
-      await ctx.reply({
-        embeds: [embed],
-        components: [
-          {
-            type: 1,
-            components: [previous, next]
-          }
-        ]
-      })
+    ])
+
+    if (!predictions.length) {
+      return await ctx.reply('commands.predictions.no_predictions')
     }
+
+    const embed = new EmbedBuilder()
+      .setAuthor({
+        name: t('commands.predictions.embed.author'),
+        iconURL: ctx.interaction.user.displayAvatarURL({ size: 2048 })
+      })
+      .setDesc(
+        t('commands.predictions.embed.desc', {
+          correct: ctx.db.profile.correctPredictions,
+          wrong: ctx.db.profile.incorrectPredictions,
+          t: count
+        })
+      )
+      .setFooter({
+        text: t('commands.predictions.embed.footer', {
+          page
+        })
+      })
+
+    for (const prediction of predictions.slice(0, 5)) {
+      let odd = ''
+      if (prediction.odd) {
+        odd += `\nOdd: \`${prediction.odd}x\``
+      }
+
+      const timestamp = (prediction.createdAt.getTime() / 1000).toFixed(0)
+
+      embed.addField(
+        `${prediction.teams[0].name} <:versus:1349105624180330516> ${prediction.teams[1].name} (<t:${timestamp}:d> <t:${timestamp}:t> | <t:${timestamp}:R>)`,
+        t('commands.predictions.embed.field', {
+          score1: prediction.teams[0].score,
+          score2: prediction.teams[1].score,
+          link: `https://www.vlr.gg/${prediction.match}`,
+          status: status[prediction.status]
+        }) + odd
+      )
+    }
+
+    const previous = new ButtonBuilder()
+      .setEmoji('1404176223621611572')
+      .setCustomId(`predictions;${ctx.interaction.user.id};${page - 1};previous;${ctx.args[0]}`)
+      .defineStyle('blue')
+
+    const next = new ButtonBuilder()
+      .setEmoji('1404176291829121028')
+      .setCustomId(`predictions;${ctx.interaction.user.id};${page + 1};next;${ctx.args[0]}`)
+      .defineStyle('blue')
+
+    if (page <= 1) {
+      previous.setDisabled()
+    }
+    if (count <= 5) {
+      next.setDisabled()
+    }
+
+    await ctx.reply({
+      embeds: [embed],
+      components: [
+        {
+          type: 1,
+          components: [previous, next]
+        }
+      ]
+    })
   },
   async createMessageComponentInteraction({ ctx, t, app }) {
-    if (ctx.args[4] === 'valorant') {
-      const predictions = await app.prisma.prediction.findMany({
+    const page = Number(ctx.args[2]) || 1
+
+    const [predictions, count] = await Promise.all([
+      app.prisma.prediction.findMany({
         where: {
-          game: 'valorant',
-          profile: {
-            userId: ctx.db.profile.userId,
-            guildId: ctx.db.guild.id
-          }
+          game: ctx.args[4] as $Enums.Game,
+          profileId: ctx.db.profile.id
         },
         include: {
           teams: true
-        }
-      })
-
-      if (!predictions.length) {
-        return await ctx.reply('commands.predictions.no_predictions')
-      }
-
-      let preds = predictions.sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())
-
-      const page = Number(ctx.args[2])
-      const pages = Math.ceil(predictions.length / 5)
-
-      preds = preds.slice(page * 5 - 5, page * 5)
-
-      if (!preds.length) {
-        return await ctx.reply('commands.predictions.no_pages')
-      }
-
-      const embed = new EmbedBuilder()
-        .setAuthor({
-          name: t('commands.predictions.embed.author'),
-          iconURL: ctx.interaction.user.displayAvatarURL({ size: 2048 })
-        })
-        .setDesc(
-          t('commands.predictions.embed.desc', {
-            correct: ctx.db.profile.correctPredictions,
-            wrong: ctx.db.profile.incorrectPredictions,
-            t: predictions.length
-          })
-        )
-        .setFooter({
-          text: t('commands.predictions.embed.footer', {
-            p1: Number.isNaN(page) ? 1 : page,
-            p2: pages
-          })
-        })
-
-      for (const prediction of preds) {
-        let status: string
-        let odd = ''
-
-        if (prediction.status === 'correct') {
-          status = '<:success:1300882212190945292>'
-        } else if (prediction.status === 'incorrect') {
-          status = '<:error:1300882259078938685>'
-        } else {
-          status = '<a:carregando:809221866434199634>'
-        }
-        if (prediction.odd) {
-          odd += `\nOdd: \`${prediction.odd}x\``
-        }
-
-        const timestamp = (prediction.createdAt.getTime() / 1000).toFixed(0)
-        embed.addField(
-          `${prediction.teams[0].name} <:versus:1349105624180330516> ${prediction.teams[1].name} (<t:${timestamp}:d> <t:${timestamp}:t> | <t:${timestamp}:R>)`,
-          t('commands.predictions.embed.field', {
-            score1: prediction.teams[0].score,
-            score2: prediction.teams[1].score,
-            link: `https://www.vlr.gg/${prediction.match}`,
-            status
-          }) + odd
-        )
-      }
-      const previous = new ButtonBuilder()
-        .setEmoji('1404176223621611572')
-        .defineStyle('blue')
-        .setCustomId(`${ctx.args[0]};${ctx.args[1]};${page - 1};previous;valorant`)
-
-      const next = new ButtonBuilder()
-        .setEmoji('1404176291829121028')
-        .defineStyle('blue')
-        .setCustomId(`${ctx.args[0]};${ctx.args[1]};${page + 1};next;valorant`)
-
-      if (page <= 1) previous.setDisabled()
-      if (page >= pages) next.setDisabled()
-
-      await ctx.edit({
-        embeds: [embed],
-        components: [
-          {
-            type: 1,
-            components: [previous, next]
-          }
-        ]
-      })
-    } else {
-      const predictions = await app.prisma.prediction.findMany({
-        where: {
-          game: 'valorant',
-          profile: {
-            userId: ctx.db.profile.userId,
-            guildId: ctx.db.guild.id
-          }
         },
-        include: {
-          teams: true
+        orderBy: {
+          createdAt: 'desc'
+        },
+        skip: (page - 1) * 5,
+        take: 5
+      }),
+      app.prisma.prediction.count({
+        where: {
+          game: ctx.args[4] as $Enums.Game,
+          profileId: ctx.db.profile.id
         }
       })
+    ])
 
-      if (!predictions.length) {
-        return await ctx.reply('commands.predictions.no_predictions')
-      }
-
-      let preds = predictions.sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())
-
-      const page = Number(ctx.args[2])
-      const pages = Math.ceil(predictions.length / 5)
-
-      preds = preds.slice(page * 5 - 5, page * 5)
-
-      if (!preds.length) {
-        return await ctx.reply('commands.predictions.no_pages')
-      }
-
-      const embed = new EmbedBuilder()
-        .setAuthor({
-          name: t('commands.predictions.embed.author'),
-          iconURL: ctx.interaction.user.displayAvatarURL({ size: 2048 })
-        })
-        .setDesc(
-          t('commands.predictions.embed.desc', {
-            correct: ctx.db.profile.correctPredictions,
-            wrong: ctx.db.profile.incorrectPredictions,
-            t: predictions.length
-          })
-        )
-        .setFooter({
-          text: t('commands.predictions.embed.footer', {
-            p1: Number.isNaN(page) ? 1 : page,
-            p2: pages
-          })
-        })
-
-      for (const prediction of preds) {
-        let status: string
-        let odd = ''
-
-        if (prediction.status === 'correct') {
-          status = '<:success:1300882212190945292>'
-        } else if (prediction.status === 'incorrect') {
-          status = '<:error:1300882259078938685>'
-        } else {
-          status = '<a:carregando:809221866434199634>'
-        }
-        if (prediction.odd) {
-          odd += `\nOdd: \`${prediction.odd}x\``
-        }
-
-        const timestamp = (prediction.createdAt.getTime() / 1000).toFixed(0)
-
-        embed.addField(
-          `${prediction.teams[0].name} <:versus:1349105624180330516> ${prediction.teams[1].name} (<t:${timestamp}:d> <t:${timestamp}:t> | <t:${timestamp}:R>)`,
-          t('commands.predictions.embed.field', {
-            score1: prediction.teams[0].score,
-            score2: prediction.teams[1].score,
-            link: `https://www.loltv.gg/match/${prediction.match}`,
-            status
-          }) + odd
-        )
-      }
-      const previous = new ButtonBuilder()
-        .setEmoji('1404176223621611572')
-        .defineStyle('blue')
-        .setCustomId(`${ctx.args[0]};${ctx.args[1]};${page - 1};previous;lol`)
-
-      const next = new ButtonBuilder()
-        .setEmoji('1404176291829121028')
-        .defineStyle('blue')
-        .setCustomId(`${ctx.args[0]};${ctx.args[1]};${page + 1};next;lol`)
-
-      if (page <= 1) previous.setDisabled()
-      if (page >= pages) next.setDisabled()
-
-      await ctx.edit({
-        embeds: [embed],
-        components: [
-          {
-            type: 1,
-            components: [previous, next]
-          }
-        ]
-      })
+    if (!predictions.length) {
+      return await ctx.reply('commands.predictions.no_predictions')
     }
+
+    const embed = new EmbedBuilder()
+      .setAuthor({
+        name: t('commands.predictions.embed.author'),
+        iconURL: ctx.interaction.user.displayAvatarURL({ size: 2048 })
+      })
+      .setDesc(
+        t('commands.predictions.embed.desc', {
+          correct: ctx.db.profile.correctPredictions,
+          wrong: ctx.db.profile.incorrectPredictions,
+          t: count
+        })
+      )
+      .setFooter({
+        text: t('commands.predictions.embed.footer', {
+          page
+        })
+      })
+
+    for (const prediction of predictions.slice(0, 5)) {
+      let odd = ''
+      if (prediction.odd) {
+        odd += `\nOdd: \`${prediction.odd}x\``
+      }
+
+      const timestamp = (prediction.createdAt.getTime() / 1000).toFixed(0)
+
+      embed.addField(
+        `${prediction.teams[0].name} <:versus:1349105624180330516> ${prediction.teams[1].name} (<t:${timestamp}:d> <t:${timestamp}:t> | <t:${timestamp}:R>)`,
+        t('commands.predictions.embed.field', {
+          score1: prediction.teams[0].score,
+          score2: prediction.teams[1].score,
+          link: `https://www.vlr.gg/${prediction.match}`,
+          status: status[prediction.status]
+        }) + odd
+      )
+    }
+
+    const previous = new ButtonBuilder()
+      .setEmoji('1404176223621611572')
+      .setCustomId(`predictions;${ctx.interaction.user.id};${page - 1};previous;${ctx.args[4]}`)
+      .defineStyle('blue')
+
+    const next = new ButtonBuilder()
+      .setEmoji('1404176291829121028')
+      .setCustomId(`predictions;${ctx.interaction.user.id};${page + 1};next;${ctx.args[4]}`)
+      .defineStyle('blue')
+
+    if (page <= 1) {
+      previous.setDisabled()
+    }
+    if (count <= 5) {
+      next.setDisabled()
+    }
+
+    await ctx.edit({
+      embeds: [embed],
+      components: [
+        {
+          type: 1,
+          components: [previous, next]
+        }
+      ]
+    })
   }
 })
