10cf2b2d4d03346ddce93bc32eb9224616fc24d9
levisantosp <levi.pires2005@gmail.com>
2026-01-19 22:01:54 -0300

    feat(utils): init

---
+++ packages/utils/src/agents.ts
@@ -0,0 +1,142 @@
+export const valorantAgents = [
+  {
+    name: 'Brimstone',
+    role: 'controller',
+    emoji: '<:brimstone:1404832324427845783>'
+  },
+  {
+    name: 'Omen',
+    role: 'controller',
+    emoji: '<:omen:1404832591378645165>'
+  },
+  {
+    name: 'Viper',
+    role: 'controller',
+    emoji: '<:viper:1404832690703962203>'
+  },
+  {
+    name: 'Raze',
+    role: 'duelist',
+    emoji: '<:raze:1404832759817699400>'
+  },
+  {
+    name: 'Cypher',
+    role: 'sentinel',
+    emoji: '<:cypher:1404832818571382985>'
+  },
+  {
+    name: 'Sage',
+    role: 'sentinel',
+    emoji: '<:sage:1404832895805427832>'
+  },
+  {
+    name: 'Sova',
+    role: 'initiator',
+    emoji: '<:sova:1404832945998401536>'
+  },
+  {
+    name: 'Phoenix',
+    role: 'duelist',
+    emoji: '<:phoenix:1404833016429154324>'
+  },
+  {
+    name: 'Jett',
+    role: 'duelist',
+    emoji: '<:jett:1404833095193985137>'
+  },
+  {
+    name: 'Breach',
+    role: 'initiator',
+    emoji: '<:breach:1404833186797715589>'
+  },
+  {
+    name: 'Reyna',
+    role: 'duelist',
+    emoji: '<:reyna:1404833273460428921>'
+  },
+  {
+    name: 'Killjoy',
+    role: 'sentinel',
+    emoji: '<:killjoy:1404833408034541669>'
+  },
+  {
+    name: 'Skye',
+    role: 'initiator',
+    emoji: '<:skye:1404833496974753802>'
+  },
+  {
+    name: 'Yoru',
+    role: 'duelist',
+    emoji: '<:yoru:1404833537773015163>'
+  },
+  {
+    name: 'Astra',
+    role: 'controller',
+    emoji: '<:astra:1404833596606386196>'
+  },
+  {
+    name: 'KAY/O',
+    role: 'initiator',
+    emoji: '<:kayo:1404833671403540582>'
+  },
+  {
+    name: 'Chamber',
+    role: 'sentinel',
+    emoji: '<:chamber:1404833713610555416>'
+  },
+  {
+    name: 'Neon',
+    role: 'duelist',
+    emoji: '<:neon:1404833902794637323>'
+  },
+  {
+    name: 'Fade',
+    role: 'initiator',
+    emoji: '<:fade:1404834161667215511>'
+  },
+  {
+    name: 'Harbor',
+    role: 'controller',
+    emoji: '<:harbor:1404834211164065903>'
+  },
+  {
+    name: 'Gekko',
+    role: 'initiator',
+    emoji: '<:gekko:1404834402998947900>'
+  },
+  {
+    name: 'Deadlock',
+    role: 'sentinel',
+    emoji: '<:deadlock:1404834466119159868>'
+  },
+  {
+    name: 'Iso',
+    role: 'duelist',
+    emoji: '<:iso:1404834503528284191>'
+  },
+  {
+    name: 'Clove',
+    role: 'controller',
+    emoji: '<:clove:1404834553390170203>'
+  },
+  {
+    name: 'Vyse',
+    role: 'sentinel',
+    emoji: '<:vyse:1404834578916704300>'
+  },
+  {
+    name: 'Tejo',
+    role: 'initiator',
+    emoji: '<:tejo:1404834612206764254>'
+  },
+  {
+    name: 'Waylay',
+    role: 'duelist',
+    emoji: '<:waylay:1404834635531161811>'
+  },
+  {
+    name: 'Veto',
+    role: 'sentinel',
+    emoji: '<:veto:1461783040320737433>'
+  }
+] as const
---
+++ packages/utils/src/weapons.ts
@@ -0,0 +1,202 @@
+export const valorantWeapons = [
+  {
+    name: 'Classic',
+    price: 0,
+    damage: {
+      head: 78,
+      chest: 26
+    },
+    magazine: 48,
+    rate_fire: 148.15 / 10
+  },
+  {
+    name: 'Shorty',
+    price: 200,
+    damage: {
+      head: 22,
+      chest: 11
+    },
+    magazine: 8,
+    rate_fire: 303.03 / 10
+  },
+  {
+    name: 'Frenzy',
+    price: 450,
+    damage: {
+      head: 78,
+      chest: 26
+    },
+    magazine: 60,
+    rate_fire: 100 / 10
+  },
+  {
+    name: 'Ghost',
+    price: 500,
+    damage: {
+      head: 105,
+      chest: 30
+    },
+    magazine: 52,
+    rate_fire: 148.15 / 10
+  },
+  {
+    name: 'Bandit',
+    price: 600,
+    damage: {
+      head: 152,
+      chest: 39
+    },
+    magazine: 32,
+    rate_fire: 240 / 10
+  },
+  {
+    name: 'Sheriff',
+    price: 800,
+    damage: {
+      head: 159,
+      chest: 55
+    },
+    magazine: 30,
+    rate_fire: 250 / 10
+  },
+  {
+    name: 'Stinger',
+    price: 950,
+    damage: {
+      head: 67,
+      chest: 27
+    },
+    magazine: 80,
+    rate_fire: 55.56 / 10
+  },
+  {
+    name: 'Spectre',
+    price: 1600,
+    damage: {
+      head: 78,
+      chest: 26
+    },
+    magazine: 120,
+    rate_fire: 75 / 10
+  },
+  {
+    name: 'Bucky',
+    price: 850,
+    damage: {
+      head: 40,
+      chest: 20
+    },
+    magazine: 15,
+    rate_fire: 909.09 / 10
+  },
+  {
+    name: 'Judge',
+    price: 1850,
+    damage: {
+      head: 34,
+      chest: 17
+    },
+    magazine: 20,
+    rate_fire: 285.71 / 10
+  },
+  {
+    name: 'Bulldog',
+    price: 2050,
+    damage: {
+      head: 115,
+      chest: 35
+    },
+    magazine: 96,
+    rate_fire: 109.29 / 10
+  },
+  {
+    name: 'Guardian',
+    price: 2250,
+    damage: {
+      head: 195,
+      chest: 65
+    },
+    magazine: 48,
+    rate_fire: 153.85 / 10
+  },
+  {
+    name: 'Phantom',
+    price: 2900,
+    damage: {
+      head: 140,
+      chest: 35
+    },
+    magazine: 90,
+    rate_fire: 90.91 / 10
+  },
+  {
+    name: 'Vandal',
+    price: 2900,
+    damage: {
+      head: 160,
+      chest: 40
+    },
+    magazine: 75,
+    rate_fire: 108.11 / 10
+  },
+  {
+    name: 'Marshal',
+    price: 950,
+    damage: {
+      head: 202,
+      chest: 101
+    },
+    magazine: 20,
+    rate_fire: 666.67 / 10
+  },
+  {
+    name: 'Operator',
+    price: 4700,
+    damage: {
+      head: 255,
+      chest: 150
+    },
+    magazine: 15,
+    rate_fire: 1333.33 / 10
+  },
+  {
+    name: 'Outlaw',
+    price: 2400,
+    damage: {
+      head: 238,
+      chest: 140
+    },
+    magazine: 12,
+    rate_fire: 666.67 / 10
+  },
+  {
+    name: 'Ares',
+    price: 1600,
+    damage: {
+      head: 75,
+      chest: 30
+    },
+    magazine: 150,
+    rate_fire: 100 / 10
+  },
+  {
+    name: 'Odin',
+    price: 3200,
+    damage: {
+      head: 95,
+      chest: 38
+    },
+    magazine: 300,
+    rate_fire: 83.33 / 10
+  },
+  {
+    name: 'Melee',
+    price: 0,
+    damage: {
+      head: 50,
+      chest: 50
+    },
+    magazine: 0,
+    rate_fire: 750 / 10
+  }
+] as const
