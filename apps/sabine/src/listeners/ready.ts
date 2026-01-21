b79bb93eca325a88cc7331a2c9987c4b9c5fd866
levisantosp <levi.pires2005@gmail.com>
2026-01-21 20:24:27 -0300

    feat(sabine): implement results processing queue for LOL and Valorant games

---
+++ apps/sabine/src/listeners/ready.ts
@@ -0,0 +1,260 @@
+import { prisma } from '@db'
+import t from '@i18n'
+import * as Discord from 'discord.js'
+import pLimit from 'p-limit'
+import { env } from '@/env'
+import type App from '@/structures/app/App'
+import createListener from '@/structures/app/createListener'
+import ButtonBuilder from '@/structures/builders/ButtonBuilder'
+import EmbedBuilder from '@/structures/builders/EmbedBuilder'
+import { type ResultsPayload, resultsQueue } from '@/structures/queue/results-queue'
+import calcOdd from '@/util/calcOdd'
+
+const tournaments: { [key: string]: RegExp[] } = {
+  'Valorant Champions Tour': [/valorant champions/, /valorant masters/, /vct \d{4}/],
+  'Valorant Challengers League': [/challengers \d{4}/],
+  'Valorant Game Changers': [/game changers \d{4}/]
+}
+
+const rest = new Discord.REST().setToken(env.BOT_TOKEN)
+
+const limit = pLimit(25)
+
+const processPredictions = async (data: ResultsPayload) => {
+  let cursor: string | undefined
+
+  while (true) {
+    const preds = await prisma.prediction.findMany({
+      take: 500,
+      skip: cursor ? 1 : 0,
+      cursor: cursor
+        ? {
+            id: cursor
+          }
+        : undefined,
+      where: {
+        game: data.game,
+        match: data.id,
+        status: 'pending'
+      },
+      include: {
+        teams: true,
+        profile: {
+          select: {
+            id: true,
+            user: {
+              select: {
+                premium: true
+              }
+            }
+          }
+        }
+      }
+    })
+
+    if (!preds.length) break
+
+    let oddA = 0
+    let oddB = 0
+    let winnerIndex = -1
+
+    const hasbets = preds.some(m => m.bet)
+
+    if (hasbets) {
+      winnerIndex = data.teams.findIndex(t => t.winner)
+
+      for (const pred of preds) {
+        if (pred.bet) {
+          if (pred.teams[0].winner) {
+            oddA++
+          } else if (pred.teams[1].winner) {
+            oddB++
+          }
+        }
+      }
+    }
+
+    const transactions = preds.flatMap(pred => {
+      if (
+        pred.teams[0].score === data.teams[0].score &&
+        pred.teams[1].score === data.teams[1].score
+      ) {
+        let odd: number | null = null
+        let bonus = 0
+
+        if (pred.bet && winnerIndex >= 0) {
+          if (pred.teams[winnerIndex].winner) {
+            odd = pred.teams[0].winner ? calcOdd(oddA) : calcOdd(oddB)
+
+            if (pred.profile.user.premium) {
+              bonus = Math.floor(Number(pred.bet) / 2)
+            }
+          }
+        }
+
+        const poisons = BigInt(Math.floor(Number(pred.bet) * (odd ?? 1))) + BigInt(bonus)
+        const fates = 35
+
+        return [
+          prisma.prediction.update({
+            where: { id: pred.id },
+            data: {
+              odd: odd ? Math.floor(odd) : undefined,
+              status: 'correct'
+            }
+          }),
+          prisma.profile.update({
+            where: { id: pred.profile.id },
+            data: {
+              correctPredictions: { increment: 1 },
+              poisons: { increment: poisons },
+              fates: { increment: fates }
+            }
+          })
+        ]
+      } else {
+        return [
+          prisma.prediction.update({
+            where: { id: pred.id },
+            data: { status: 'incorrect' }
+          }),
+          prisma.profile.update({
+            where: { id: pred.profile.id },
+            data: {
+              incorrectPredictions: { increment: 1 }
+            }
+          })
+        ]
+      }
+    })
+
+    await prisma.$transaction(transactions)
+    cursor = preds[preds.length - 1].id
+  }
+}
+
+const processResult = async (app: App, data: ResultsPayload) => {
+  const matchedEventNames = Object.keys(tournaments).filter(
+    key =>
+      key.toLowerCase() === data.tournament.name ||
+      tournaments[key].some(regex => regex.test(data.tournament.name))
+  )
+
+  if (!matchedEventNames.includes(data.tournament.name)) {
+    matchedEventNames.push(data.tournament.name)
+  }
+
+  let cursor: string | undefined
+
+  while (true) {
+    const guilds = await prisma.guild.findMany({
+      take: 1000,
+      skip: cursor ? 1 : 0,
+      cursor: cursor
+        ? {
+            id: cursor
+          }
+        : undefined,
+      where: {
+        events: {
+          some: {
+            type: data.game,
+            name: {
+              in: matchedEventNames
+            }
+          }
+        }
+      },
+      select: {
+        id: true,
+        lang: true,
+        events: {
+          where: {
+            type: data.game,
+            name: {
+              in: matchedEventNames
+            }
+          },
+          select: {
+            name: true,
+            channel2: true
+          }
+        }
+      }
+    })
+
+    if (!guilds.length) break
+
+    const messages: Promise<unknown>[] = []
+
+    for (const guild of guilds) {
+      if (
+        (!guild.events.some(e => e.name === data.tournament.name) &&
+          !guild.events.some(e =>
+            tournaments[e.name]?.some(regex =>
+              regex.test(data.tournament.name.replace(/\s+/g, ' ').trim().toLowerCase())
+            )
+          )) ||
+        !guild.events[0]
+      )
+        continue
+
+      const emoji1 =
+        app.emoji.get(data.teams[0].name.toLowerCase()) ??
+        app.emoji.get(app.emojiAliases.get(data.teams[0].name.toLowerCase()) ?? '') ??
+        app.emoji.get('default')
+      const emoji2 =
+        app.emoji.get(data.teams[1].name.toLowerCase()) ??
+        app.emoji.get(app.emojiAliases.get(data.teams[1].name.toLowerCase()) ?? '') ??
+        app.emoji.get('default')
+
+      const embed = new EmbedBuilder()
+        .setAuthor({
+          name: data.tournament.name,
+          iconURL: data.tournament.image
+        })
+        .setField(
+          `${emoji1} ${data.teams[0].name} \`${data.teams[0].score}\` <:versus:1349105624180330516> \`${data.teams[1].score}\` ${data.teams[1].name} ${emoji2}`,
+          `<t:${data.when.getTime() / 1000}:F> | <t:${data.when.getTime() / 1000}:R>`,
+          true
+        )
+        .setFooter({ text: data.stage })
+
+      messages.push(
+        limit(() =>
+          rest.post(Discord.Routes.channelMessages(guild.events[0].channel2), {
+            body: {
+              embeds: [embed.toJSON()],
+              components: [
+                {
+                  type: 1,
+                  components: [
+                    new ButtonBuilder()
+                      .setLabel(t(guild.lang, 'helper.stats'))
+                      .defineStyle('link')
+                      .setURL(`https://vlr.gg/${data.id}`)
+                  ]
+                }
+              ]
+            }
+          })
+        )
+      )
+    }
+
+    await Promise.allSettled(messages)
+    cursor = guilds[guilds.length - 1].id
+  }
+}
+
+export default createListener({
+  name: 'clientReady',
+  async run(app) {
+    if (app.shard?.ids[0]) return
+
+    await resultsQueue.process(async job => {
+      await processPredictions(job.data)
+      await processResult(app, job.data)
+    })
+  }
+})
