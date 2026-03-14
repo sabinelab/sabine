fda816c13fb0b7ab690d4eb1330d78d5f5582626
levisantosp <levi.pires2005@gmail.com>
2026-03-14 14:49:46 -0300

    fix: improve authorization error handling and formatting

--- apps/api/src/utils/auth.ts
+++ apps/api/src/utils/auth.ts
@@ -2,10 +2,14 @@ import type { preHandlerMetaHookHandler } from "fastify/types/hooks.js"
 import { env } from "@/env"
 
 export const auth: preHandlerMetaHookHandler = (req, res, done) => {
-	if (env.NODE_ENV === "dev") return done()
-
+	if (env.NODE_ENV === "dev") {
+		return done()
+	}
+	if (!req.headers.authorization) {
+		return res.status(401).send({ message: "Unauthorized" })
+	}
 	if (req.headers.authorization !== env.AUTH) {
-		return res.status(401).send({ error: "Forbidden" })
+		return res.status(403).send({ message: "Forbidden" })
 	}
 
 	return done()
