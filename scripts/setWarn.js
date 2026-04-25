42e956ac4e72eb096e9288fdc8e1fb253bc0bfb6
levisantosp <levi.pires2005@gmail.com>
2026-04-25 18:25:49 -0300

    feat: enhance user warning update logic to process in batches

--- scripts/setWarn.js
+++ scripts/setWarn.js
@@ -1,9 +1,35 @@
-import { prisma } from '../packages/prisma'
+import { prisma } from '../prisma'
 
 export default async function () {
-  await prisma.user.updateMany({
-    data: {
-      warn: true
-    }
-  })
+  let cursor = undefined
+  while (true) {
+    const users = await prisma.user.findMany({
+      where: {
+        warn: false
+      },
+      take: 1000,
+      cursor: cursor
+        ? {
+            id: cursor
+          }
+        : cursor,
+      select: {
+        id: true
+      }
+    })
+    if (!users.length) break
+
+    await prisma.user.updateMany({
+      where: {
+        id: {
+          in: users.map((user) => user.id)
+        }
+      },
+      data: {
+        warn: true
+      }
+    })
+
+    cursor = users.at(-1)?.id
+  }
 }
