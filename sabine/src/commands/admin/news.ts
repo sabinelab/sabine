cb00f124fb9cd827c18c43c8880acafe255b49b6
levisantosp <levi.pires2005@gmail.com>
2026-05-08 20:22:38 -0300

    refactor: remove League of Legends news feature from admin commands

--- sabine/src/commands/admin/news.ts
+++ sabine/src/commands/admin/news.ts
@@ -45,28 +45,6 @@ export default createCommand({
               required: true
             }
           }
-        },
-        lol: {
-          type: ApplicationCommandOptionType.Subcommand,
-          name: 'lol',
-          description: 'Enable League of Legends news feature',
-          descriptionLocalizations: {
-            'pt-BR': 'Habilita a funcionalidade de notícias de League of Legends'
-          },
-          args: {
-            channel: {
-              type: ApplicationCommandOptionType.Channel,
-              name: 'channel',
-              nameLocalizations: {
-                'pt-BR': 'canal'
-              },
-              description: 'Enter a channel',
-              descriptionLocalizations: {
-                'pt-BR': 'Informe o canal'
-              },
-              required: true
-            }
-          }
         }
       }
     },
@@ -88,26 +66,13 @@ export default createCommand({
           descriptionLocalizations: {
             'pt-BR': 'Desabilita a funcionalidade de notícias de VALORANT'
           }
-        },
-        lol: {
-          type: ApplicationCommandOptionType.Subcommand,
-          name: 'lol',
-          description: 'Disable League of Legends news feature',
-          descriptionLocalizations: {
-            'pt-BR': 'Desabilita a funcionalidade de notícias de League of Legends'
-          }
         }
       }
     }
   },
   permissions: ['ManageChannels'],
-  syntaxes: [
-    'news enable valorant [channel]',
-    'news enable lol [channel]',
-    'news disable valorant',
-    'news disable lol'
-  ],
-  examples: ['news enable valorant #news', 'news enable lol #lol-news', 'news disable valorant', 'news disable lol'],
+  syntaxes: ['news enable valorant [channel]', 'news disable valorant'],
+  examples: ['news enable valorant #news', 'news disable valorant'],
   async run({ ctx }) {
     if (ctx.args.enable) {
       const games = {
@@ -131,31 +96,10 @@ export default createCommand({
           await ctx.reply('commands.news.news_enabled', {
             ch: channel.toString()
           })
-        },
-        lol: async () => {
-          const channel = ctx.guild.channels.cache.get(ctx.args.enable?.lol?.channel.id ?? '')!
-
-          if (![0, 5].some((t) => t === channel.type)) return await ctx.reply('commands.news.invalid_channel')
-
-          await prisma.guild.upsert({
-            where: {
-              id: ctx.guild.id
-            },
-            update: {
-              lolNewsChannel: channel.id
-            },
-            create: {
-              id: ctx.guild.id,
-              lolNewsChannel: channel.id
-            }
-          })
-          await ctx.reply('commands.news.news_enabled', {
-            ch: channel.toString()
-          })
         }
       }
 
-      const game = Object.keys(ctx.args.enable)[0] as 'valorant' | 'lol'
+      const game = Object.keys(ctx.args.enable)[0] as 'valorant'
       await games[game]()
     } else {
       const games = {
@@ -172,25 +116,11 @@ export default createCommand({
             }
           })
           await ctx.reply('commands.news.news_disabled')
-        },
-        lol: async () => {
-          await prisma.guild.upsert({
-            where: {
-              id: ctx.db.guild.id
-            },
-            update: {
-              lolNewsChannel: null
-            },
-            create: {
-              id: ctx.db.guild.id
-            }
-          })
-          await ctx.reply('commands.news.news_disabled')
         }
       }
 
       if (ctx.args.disable) {
-        const game = Object.keys(ctx.args.disable)[0] as 'valorant' | 'lol'
+        const game = Object.keys(ctx.args.disable)[0] as 'valorant'
         await games[game]()
       }
     }
