804c2627d1f02394a35ce5a7766fd83bf0eaf3a4
levisantosp <levi.pires2005@gmail.com>
2026-05-10 19:44:02 -0300

    feat: add esports world cup tournament

--- sabine/src/config.ts
+++ sabine/src/config.ts
@@ -1,5 +1,6 @@
 export const tournaments: { [key: string]: RegExp[] } = {
   'Valorant Champions Tour': [/valorant champions/, /valorant masters/, /vct \d{4}/],
   'Valorant Challengers League': [/challengers \d{4}/, /vct \d{4}: ascension/],
-  'Valorant Game Changers': [/game changers \d{4}/]
+  'Valorant Game Changers': [/game changers \d{4}/],
+  'Esports World Cup': [/esports world cup \d{4}/]
 }
