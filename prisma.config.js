c63d344c86e565dc00077e7f36311345d79c0f17
levisantosp <levi.pires2005@gmail.com>
2026-05-19 10:43:58 -0300

    chore: update monorepo structure

--- prisma.config.js
+++ prisma.config.js
@@ -1,7 +1,7 @@
 import { defineConfig, env } from 'prisma/config'
 
 export default defineConfig({
-  schema: 'prisma/src/schemas/',
+  schema: 'packages/prisma/src/schemas/',
   datasource: {
     url: env('DATABASE_URL')
   }
